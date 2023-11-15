a5cce2e152a676c1e0eeb9f39d0ebb30ecc6803a
ali <mustafaevalibek52@gmail.com>
2023-11-15 16:07:42 +0500

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+// let ask = prompt('Your name?')
+
+// if (ask.length % 2 === 0) {
+//     console.log('Come in');
+// } else {
+//     console.log('Go away');
+// }
+
+
+
+let name = 'Ali'
+let momey = 10000
+let account = 7777
+let ask = prompt('Введите имя для снятие средств')
+
+if (name.toLowerCase() == ask.toLowerCase()) {
+    let 
+} else {
+    console.log('пользователь не найден');
+}
